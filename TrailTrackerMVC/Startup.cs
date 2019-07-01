50644c54fa7e62133b8e67cd849227882e77c66a
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-07-01 11:06:29 -0400

    first commit

---
+++ TrailTrackerMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrailTrackerMVC.Startup))]
+namespace TrailTrackerMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
